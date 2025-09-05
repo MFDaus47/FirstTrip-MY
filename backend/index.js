eef75f1692df114e52f3ee4df543ba1bef4e9d7c
MFDaus47 <mfdaus3158@gmail.com>
2025-09-05 23:18:28 +0800

    created Test page for DB Connection to test wheteher DB connected successfully

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db.config");
 const testRoute = require("./routes/test");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
 app.use("/api/test", testRoute);
 
 const PORT = process.env.PORT;
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-    app.listen(PORT, () => console.log("Server running on port ${PORT"));
-  })
-  .catch((err) => console.error(err));
+app.listen(PORT, () => {
+  console.log("Server is running on port ${PORT}");
+});
