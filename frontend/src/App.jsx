82ac8102d3073bf059b6439b8aef5a74f1be137c
MFDaus47 <mfdaus3158@gmail.com>
2025-09-07 11:49:38 +0800

    Added resizable Navbar from aceternity for homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,73 @@
 import "./App.css";
 import TripForm from "@/components/TripForm";
 import { useState } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  MobileNavHeader,
+  MobileNavMenu,
+  MobileNavToggle,
+  NavbarLogo,
+  NavbarButton,
+} from "./components/ui/resizable-navbar";
 
 function App() {
   const [result, setResult] = useState(null);
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const navItems = [
+    {
+      name: "Home",
+      link: "#",
+    },
+    {
+      name: "About",
+      link: "#",
+    },
+    {
+      name: "Contact",
+      link: "#",
+    },
+  ];
 
   return (
-    <>
-      <div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="bg-background">
+        <div>
+          <Navbar>
+            {/* Desktop Navbar */}
+            <NavBody>
+              <NavbarLogo />
+              <NavItems items={navItems} />
+              <NavbarButton href="#">Book Now</NavbarButton>
+            </NavBody>
+
+            {/* Mobile Navbar */}
+            <MobileNav>
+              <MobileNavHeader>
+                <NavbarLogo />
+                <MobileNavToggle
+                  isOpen={mobileOpen}
+                  onClick={() => setMobileOpen(!mobileOpen)}
+                />
+              </MobileNavHeader>
+              <MobileNavMenu
+                isOpen={mobileOpen}
+                onClose={() => setMobileOpen(false)}
+              >
+                <NavItems
+                  items={navItems}
+                  onItemClick={() => setMobileOpen(false)}
+                />
+                <NavbarButton href="#">Book Now</NavbarButton>
+              </MobileNavMenu>
+            </MobileNav>
+          </Navbar>
+        </div>
+
         <h1>FirstTrip-MY: Trip Cost Estimator</h1>
         <TripForm setResult={setResult} />
 
@@ -21,7 +81,7 @@ function App() {
           </div>
         )}
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
